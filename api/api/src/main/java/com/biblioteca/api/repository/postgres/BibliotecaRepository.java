d20367a63053cc159ba96c2549878081f395d7ba
Linejesus <161092862+Linejesus@users.noreply.github.com>
2026-05-19 19:58:30 +0000

    Frontend finalizado

--- api/api/src/main/java/com/biblioteca/api/repository/postgres/BibliotecaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.api.repository.postgres;
-
-import com.biblioteca.api.entity.postgres.Biblioteca;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BibliotecaRepository extends JpaRepository<Biblioteca, Long> {
-}
--- api/api/src/main/java/com/biblioteca/api/repository/postgres/UsuariosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.biblioteca.api.repository.postgres;
-
-import com.biblioteca.api.entity.postgres.Usuarios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
-    
-}
